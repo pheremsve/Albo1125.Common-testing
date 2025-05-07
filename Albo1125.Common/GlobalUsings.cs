782dbcac67ea4d311d1246de970c6557686ee634
pheremsve <148281394+pheremsve@users.noreply.github.com>
2025-05-07 16:00:58 -0600

    Add files via upload

---
+++ Albo1125.Common/GlobalUsings.cs
@@ -0,0 +1,37 @@
+﻿// Global using directives
+
+global using System.Text;
+global using System.Drawing;
+global using System.Windows.Forms;
+global using System.Globalization;
+global using System.IO;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Diagnostics;
+global using System.Runtime.InteropServices;
+global using System.Xml;
+global using System.Reflection;
+global using System.Net;
+global using System.Xml.Linq;
+global using System.ComponentModel;
+global using System.IO.Ports;
+global using Microsoft.Win32;
+global using System.Threading.Tasks;
+
+// RPH Related
+global using LSPD_First_Response.Mod.API;
+global using LSPD_First_Response.Engine.Scripting.Entities;
+global using Rage;
+global using Rage.Native;
+
+// Albo1125 place holder
+global using Albo1125.Common.CommonLibrary;
+global using static Albo1125.Common.UpdateEntry;
+global using static Albo1125.Common.CommonLibrary.ExtensionMethods;
+
+
+
+
+
